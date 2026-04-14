d163e8a56e44815d5e44a2d46a99353866184a2f
Muhammad Aqsa Firdaus <maqsafirdaus1@gmail.com>
2026-04-14 16:15:56 +0700

    Update data file path for dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,7 +7,7 @@ import matplotlib.ticker as ticker
 sns.set(style='whitegrid')
 
 # LOAD DATA
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("dashboard/all_data.csv")
 all_df["order_purchase_timestamp"] = pd.to_datetime(all_df["order_purchase_timestamp"])
 
 # SIDEBAR
